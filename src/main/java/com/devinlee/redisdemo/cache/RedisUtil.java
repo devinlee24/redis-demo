94614551c3cd326e713b563e3c2c524975f58508
devinlee <devinlee24@foxmail.com>
2019-12-11 17:55:17 +0800

    Redis初始化设置

--- src/main/java/com/devinlee/redisdemo/cache/RedisUtil.java
+++ src/main/java/com/devinlee/redisdemo/cache/RedisUtil.java
@@ -3,7 +3,6 @@ package com.devinlee.redisdemo.cache;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
