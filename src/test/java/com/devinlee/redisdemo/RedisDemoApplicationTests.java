8c190a0d0e4c65837849fb746c4fe7674c838d52
devinlee <devinlee24@foxmail.com>
2019-12-11 16:35:59 +0800

    初始化

---
+++ src/test/java/com/devinlee/redisdemo/RedisDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devinlee.redisdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
